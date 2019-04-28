3507eb6625adab1bf65c6d71da867c57e00829cc
upmuwei <1923162013@qq.com>
2019-04-28 17:14:17 +0800

    添加MD5加盐加密

---
+++ src/main/java/com/expressTracking/utils/MD5Utils.java
@@ -0,0 +1,62 @@
+package com.expressTracking.utils;
+
+import java.util.Random;
+
+import static org.apache.commons.codec.digest.DigestUtils.md5Hex;
+
+/**
+ * 密码加解密工具类、
+ * @author muwei
+ * @date 2019/4/28
+ */
+public class MD5Utils {
+
+    /**
+     * MD5加盐加密
+     * @param password 密码
+     * @return 加密后字符
+     */
+    public static String getSaltMD5(String password) {
+        // 生成一个16位的随机数
+        Random random = new Random();
+        StringBuilder sBuilder = new StringBuilder(16);
+        sBuilder.append(random.nextInt(99999999)).append(random.nextInt(99999999));
+        int len = sBuilder.length();
+        if (len < 16) {
+            for (int i = 0; i < 16 - len; i++) {
+                sBuilder.append("0");
+            }
+        }
+        // 生成最终的加密盐
+        String salt = sBuilder.toString();
+        password = md5Hex(password + salt);
+        char[] cs = new char[48];
+        for (int i = 0; i < 48; i += 3) {
+            cs[i] = password.charAt(i / 3 * 2);
+            char c = salt.charAt(i / 3);
+            cs[i + 1] = c;
+            cs[i + 2] = password.charAt(i / 3 * 2 + 1);
+        }
+        return String.valueOf(cs);
+    }
+
+    /**
+     * 验证加盐后是否和原文一致
+     */
+    public static boolean getSaltverifyMD5(String password, String md5str) {
+        char[] cs1 = new char[32];
+        char[] cs2 = new char[16];
+        for (int i = 0; i < 48; i += 3) {
+            cs1[i / 3 * 2] = md5str.charAt(i);
+            cs1[i / 3 * 2 + 1] = md5str.charAt(i + 2);
+            cs2[i / 3] = md5str.charAt(i + 1);
+        }
+        String salt = new String(cs2);
+        return md5Hex(password + salt).equals(String.valueOf(cs1));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MD5Utils.getSaltMD5("1"));
+    }
+
+}
